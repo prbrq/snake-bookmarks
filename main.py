25a0866183c41ba3df82efb3706c7e0c77be1dab
Arkady Chelyadnikov <prbrq@ya.ru>
2026-03-05 22:41:41 +0300

    FastAPI and Uvicorn added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def main():
-    print("Hello from snake-bookmarks!")
+from fastapi import FastAPI
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def root():
+    return {"message": "Hello, World!"}
