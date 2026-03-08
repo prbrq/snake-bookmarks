8198904f7496819abc20b52e88d9058c938f5126
Arkady Chelyadnikov <prbrq@ya.ru>
2026-03-08 09:28:43 +0300

    redirect root to docs added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from sqlmodel import SQLModel, Field, create_engine, Session, select 
 from datetime import datetime
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 
@@ -31,7 +32,13 @@ class BookmarkCreate(SQLModel):
 SQLModel.metadata.create_all(engine)
 
 @app.post("/bookmarks")
-def create_bookmark(bookmark: Bookmark):
+def create_bookmark(bookmark_create: BookmarkCreate):
+    bookmark = Bookmark(
+        url = bookmark_create.url,
+        title=bookmark_create.title,
+        description=bookmark_create.description
+    )
+
     with Session(engine) as session:
         session.add(bookmark)
         session.commit()
@@ -42,3 +49,7 @@ def create_bookmark(bookmark: Bookmark):
 # def get_items():
 #     with Session(engine) as session:
 #         return session.exec(select(Item)).all()
+
+@app.get("/", include_in_schema=False)
+def root():
+    return RedirectResponse(url="/docs")
